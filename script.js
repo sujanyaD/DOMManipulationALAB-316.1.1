c47d43eaa412e8c2c09a7764633af08186a87416
sujanyaD <soujanya.gh@gmail.com>
2024-04-25 22:23:44 -0400

    created menu bar

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-var menuLinks = [
-    { text: "about", href: "/about" },
-    { text: "catalog", href: "/catalog" },
-    { text: "orders", href: "/orders" },
-    { text: "account", href: "/account" },
+ var menuLinks = [
+ { text: "about", href: "/about" },
+ { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+   { text: "account", href: "/account" },
   ];
-  
-  import "./styles.css";
-  const mainEl = document.querySelector("main");
-  console.log(mainEl);
-  mainEl.style.background = "var(--main-bg)";
-  mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
-  mainEl.classList = "flex-ctr";
-  const topMenuE1 = document.getElementById("top-menu");
-  topMenuE1.style.height = "100%";
-  topMenuE1.style.backgroundColor = "var(--top-menu-bg)";
-  topMenuE1.classList = "flex-around";
-  menuLinks.forEach((link, i) => {
-    const aE1 = document.createElement("a");
-    aE1.textContent = "link[i].text";
-  });
+  //1.Getting Started
+
+
+ const mainEl = document.querySelector("main");
+ console.log(mainEl);
+   mainEl.style.background = "var(--main-bg)";
+   mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+ mainEl.classList.add ("flex-ctr");
+ mainEl.innerHTML="<h1>DOM Manipulation</h1>"
+ mainEl.classList.add("flex-ctr");
+
+//2.Menu Bar
+const topMenuE1 = document.getElementById("top-menu");
+topMenuE1.style.height = "100%";
+topMenuE1.style.backgroundColor = "var(--top-menu-bg)";
+topMenuE1.classList = "flex-around";
+
+//3. Addding Menu Buttons
+// Menu data structure
+
+menuLinks.forEach((link, i) => {
+  const aE1 = document.createElement("aE1");
+  aE1.setAttribute("href",menuLinks[i].href);
+  aE1.textContent=menuLinks[i].text;
+  topMenuE1.appendChild(aE1)
+});
+
+//Part 4 Adding Interactivity
+
+
+
+
+
+
   
